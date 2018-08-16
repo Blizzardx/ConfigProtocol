d15626fa896b9e6d89e203157832a84baa20818e
baoxue <blizzard_xc@126.com>
2018-08-16 19:40:02 +0800

    add csharp runtime

---
+++ runtime/csharpRuntime/csharpRuntime/core/ConfigProtoSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Config;
+using Google.Protobuf;
+
+namespace ConfigProto.core
+{
+    public class ConfigProtoSerializer
+    {
+        public Object DeSerialize(byte[] configContent,out string error)
+        {
+            error = "";
+            Config.ConfigTable config = new ConfigTable();
+
+            try
+            {
+                config.MergeFrom(configContent);
+            }
+            catch (Exception e)
+            {
+                error = "error on parser config content " + e.Message;
+                return null;
+            }
+            
+            
+            return null;
+        }
+    }
+}
